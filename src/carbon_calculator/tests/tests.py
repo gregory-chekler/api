c4069d120735b51de281ba45e8ae82905fc639cf
Gregory Chekler <gregchekler21@gmail.com>
2020-04-15 18:19:57 -0400

    fixes messages (may need to undo)

--- src/carbon_calculator/tests/tests.py
+++ src/carbon_calculator/tests/tests.py
@@ -1,9 +1,7 @@
 from django.test import TestCase, Client
 from carbon_calculator.models import CalcUser, Event, Station, Action, Group, Question
 from carbon_calculator.views import importcsv
-from database.models import Vendor
-
-#from carbon_calculator.carbonCalculator import CarbonCalculator
+from carbon_calculator.carbonCalculator import CarbonCalculator
 
 IMPORT_SUCCESS = {"status": True}
 # Create your tests here.
@@ -21,23 +19,25 @@ class CarbonCalculatorTest(TestCase):
         #Animal.objects.create(name="cat", sound="meow")
 
     def importdata(self):
+        response = self.client.post('/cc/import', { "Confirm": "Yes", "Questions":"carbon_calculator/content/Questions.csv"})
+        #self.assertEqual(response, IMPORT_SUCCESS)
 
-        response = self.client.post('/cc/import', 
-            {   "Confirm": "Yes", 
-                "Actions":"carbon_calculator/content/Actions.csv", 
-                "Questions":"carbon_calculator/content/Questions.csv",
-                "Stations":"carbon_calculator/content/Stations.csv",
-                "Groups":"carbon_calculator/content/Groups.csv",
-                "Events":"carbon_calculator/content/Events.csv"
-                })
-        self.assertEqual(response.status_code, 200)
+        response = self.client.post('/cc/import', { "Confirm": "Yes", "Actions":"carbon_calculator/content/Actions.csv"})
+        #self.assertEqual(response, IMPORT_SUCCESS)
 
-        response = self.client.post('/cc/import', 
-            {   "Confirm": "Yes", 
-                "Defaults":"carbon_calculator/content/exportdefaults.csv" 
-                })
-        self.assertEqual(response.status_code, 200)
+        response = self.client.post('/cc/import', { "Confirm": "Yes", "Stations":"carbon_calculator/content/Stations.csv"})
+        #self.assertEqual(response, IMPORT_SUCCESS)
 
-    def test_info_actions(self):
+        response = self.client.post('/cc/import', { "Confirm": "Yes", "Groups":"carbon_calculator/content/Groups.csv"})
+        #self.assertEqual(response, IMPORT_SUCCESS)
+
+        response = self.client.post('/cc/import', { "Confirm": "Yes", "Events":"carbon_calculator/content/Events.csv"})
+        #self.assertEqual(response, IMPORT_SUCCESS)
+
+    def test_EnergyFair(self):
         response = self.client.post('/cc/info/actions')
         self.assertEqual(response.status_code, 200)
+
+    def test_CommunitySolar(self):
+        response = self.client.post('/cc/info/actions')
+        self.assertEqual(response.status_code, 34232)
